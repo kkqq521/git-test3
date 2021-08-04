0b156970a52f93f9c5ea896d3bc3bb6d0df42e49
kk <kk@55555kl.com>
2021-08-04 15:23:16 +0800

    first commit

---
+++ src/main/java/com/kkqq/GitTest.java
@@ -0,0 +1,12 @@
+package com.kkqq;
+
+/**
+ * @author wk_kl
+ * @create 2021-08-04  15:20
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
